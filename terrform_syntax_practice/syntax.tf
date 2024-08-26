40688d5c0d4134eb073976d2688daa0b79047186
Kory Palmer <kory232323@gmail.com>
2024-08-26 12:21:40 -0500

    practicing syntax

--- terrform_syntax_practice/syntax.tf
+++ terrform_syntax_practice/syntax.tf
@@ -8,5 +8,10 @@ terraform {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  
+  bucket_name = var.bucket_name
 }
+
+data "aws_s3_bucket" "my_external_bucket" {
+    bucket = "not-managed-by-us"
+}
+
